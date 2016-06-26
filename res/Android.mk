44928972b28dfe2c88835e2efe96ea3441472a22
Griffin Millender <griffinn.millender@gmail.com>
2016-06-25 20:01:33 -0500

    SlimFramework: Updates for public resources
      * Use custom resource id
      * set coreApp
      * add some missing flags
    
    Change-Id: Ib26cc287fc0807a91022139158c06d7c7c02bba5

--- res/Android.mk
+++ res/Android.mk
@@ -4,14 +4,19 @@ include $(CLEAR_VARS)
 #LOCAL_NO_STANDARD_LIBRARIES := true
 LOCAL_PACKAGE_NAME := org.slim.framework-res
 LOCAL_CERTIFICATE := platform
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
+
+# 0x3f/one less than app id
+LOCAL_AAPT_FLAGS += -x 55
 
 LOCAL_MODULE_TAGS := optional
 
-# Install this alongside the libraries.
-LOCAL_MODULE_PATH := $(TARGET_OUT_JAVA_LIBRARIES)
-
 # frameworks resource packages don't like the extra subdir layer
 LOCAL_IGNORE_SUBDIR := true
+
+# Install this alongside the libraries.
+LOCAL_MODULE_PATH := $(TARGET_OUT_JAVA_LIBRARIES)
 
 # Create package-export.apk, which other packages can use to get
 # PRODUCT-agnostic resource data like IDs and type definitions.
