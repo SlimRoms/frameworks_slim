4d75c9d79322ed6320a5cbff305b3ea1d82b913e
Griffin Millender <griffinn.millender@gmail.com>
2016-09-10 23:44:02 -0500

    Use new --private-symbols aapt flag
      * private-symbols attribute is deprecated
    
    Change-Id: Ie8f6232e5608ad158f89c3f22ff87bb853da8cf1

--- res/Android.mk
+++ res/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := org.slim.framework-res
 LOCAL_CERTIFICATE := platform
 LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --private-symbols org.slim.framework.internal
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
 
 # 0x3f/one less than app id
