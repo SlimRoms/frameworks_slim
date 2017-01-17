9e04d366cc4c2968d84365eb73a90086054555d9
Griffin Millender <griffinn.millender@gmail.com>
2017-01-17 15:28:53 -0600

    Add SlimActionsPreference to handle selecting custom actions
    Change-Id: Ic51bb09c20aac9d8d81c12920220eb8c359ed2f1

---
+++ packages/SlimSettings/src/com/slim/settings/activities/ShortcutPickerActivity.java
@@ -0,0 +1,35 @@
+package com.slim.settings.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.UserHandle;
+
+import slim.utils.ShortcutPickerHelper;
+
+public class ShortcutPickerActivity extends Activity implements
+        ShortcutPickerHelper.OnPickListener {
+
+    ShortcutPickerHelper mPicker;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        mPicker = new ShortcutPickerHelper(this, this);
+        mPicker.pickShortcut(0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mPicker.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void shortcutPicked(String uri, String friendlyName, Bitmap bmp, boolean isApplication) {
+        Intent intent = new Intent(ShortcutPickerHelper.ACTION_SHORTCUT_PICKED);
+        intent.putExtra(ShortcutPickerHelper.EXTRA_ACTION, uri);
+        intent.putExtra(ShortcutPickerHelper.EXTRA_DESCRIPTION, friendlyName);
+        sendBroadcastAsUser(intent, UserHandle.CURRENT);
+        finish();
+    }
+}
