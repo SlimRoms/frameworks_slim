47f8c9b609282c744b70f7403ae0a39a91799010
Griffin Millender <griffinn.millender@gmail.com>
2017-01-22 16:46:21 -0600

    Add search to slimsettings
    Change-Id: Ib16a7d5653e2b57fffcf916eca5f73e86418c954

--- packages/SlimSettings/src/com/slim/settings/SettingsActivity.java
+++ packages/SlimSettings/src/com/slim/settings/SettingsActivity.java
@@ -19,6 +19,9 @@ package com.slim.settings;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.preference.Preference;
 import android.support.v14.preference.PreferenceFragment;
@@ -26,17 +29,71 @@ import android.support.v7.preference.PreferenceScreen;
 import android.view.MenuItem;
 
 import com.slim.settings.Utils;
+import com.slim.settings.search.SettingsList;
+import com.slim.settings.search.SettingsList.SettingInfo;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 
 public class SettingsActivity extends SettingsDrawerActivity implements
         PreferenceFragment.OnPreferenceStartFragmentCallback,
         PreferenceFragment.OnPreferenceStartScreenCallback {
 
+    public static final String META_DATA_KEY_FRAGMENT_CLASS =
+            "com.slim.settings.FRAGMENT_CLASS";
+    public static final String META_DATA_KEY_PREFRERENCE_XML =
+            "com.slim.settings.PREFERENCE_XML";
+
+    public static final String EXTRA_SHOW_FRAGMENT = ":settings:show_fragment";
+    public static final String EXTRA_SHOW_FRAGMENT_ARGUMENTS = ":settings:show_fragment_args";
+    public static final String EXTRA_SHOW_FRAGMENT_TITLE = ":settings:show_fragment_title";
+    public static final String EXTRA_FRAGMENT_ARG_KEY = ":settings:fragment_args_key";
+    public static final String EXTRA_SHOW_FRAGMENT_TITLE_RESID =
+            ":settings:show_fragment_title_resid";
+
+    private CharSequence mInitialTitle;
+    private String mFragmentClass;
+    private String mPreferenceXML;
+
+    private boolean mHomeUp = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getMetaData();
+
+        String action = getIntent().getAction();
+        if (action != null && action.startsWith(SettingsList.SETTINGS_ACTION)) {
+            android.util.Log.d("TEST", "action - " + action);
+            String key = action.substring(SettingsList.SETTINGS_ACTION.length() + 1);
+            if (key.contains("..")) {
+                key = key.substring(0, key.indexOf(".."));
+            }
+            SettingInfo info = SettingsList.get(this).getSetting(key);
+            setTitle(getIntent(), info);
+            mFragmentClass = info.fragmentClass;
+            mPreferenceXML = getResources().getResourceEntryName(info.xmlResId);
+
+        }
+
+        //if (TextUtils.isEmpty(mPreferenceXML)) {
+        //}
+
         Fragment fragment = getFragment();
+        String fragmentClass = getIntent().getStringExtra(EXTRA_SHOW_FRAGMENT);
+        if (mFragmentClass != null) {
+            fragment = Fragment.instantiate(this, mFragmentClass);
+            mHomeUp = true;
+        } else if (fragmentClass != null) {
+            fragment = Fragment.instantiate(this, fragmentClass);
+        }
+
+        Bundle initialArgs = getIntent().getBundleExtra(EXTRA_SHOW_FRAGMENT_ARGUMENTS);
+        if (initialArgs == null) {
+            initialArgs = getFragmentBundle();
+        } else {
+            initialArgs.putString("preference_xml", mPreferenceXML);
+        }
+
         if (fragment != null) {
             FragmentTransaction transaction = getFragmentManager().beginTransaction();
             if (getFragmentBundle() != null) {
@@ -47,6 +104,31 @@ public class SettingsActivity extends SettingsDrawerActivity implements
             showMenuIcon();
         }
         getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (!super.onOptionsItemSelected(item)) {
+            if (item.getItemId() == android.R.id.home) {
+                onBackPressed();
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    private void getMetaData() {
+        try {
+            ActivityInfo ai = getPackageManager().getActivityInfo(getComponentName(),
+                    PackageManager.GET_META_DATA);
+            if (ai == null || ai.metaData == null) return;
+            mFragmentClass = ai.metaData.getString(META_DATA_KEY_FRAGMENT_CLASS);
+            mPreferenceXML = ai.metaData.getString(META_DATA_KEY_PREFRERENCE_XML);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
     }
 
     @Override
@@ -102,6 +184,25 @@ public class SettingsActivity extends SettingsDrawerActivity implements
     }
 
     public Bundle getFragmentBundle() {
-        return null;
+        if (!TextUtils.isEmpty(mPreferenceXML)) {
+            Bundle b = new Bundle();
+            b.putString("preference_xml", mPreferenceXML);
+            return b;
+        }
+        return new Bundle();
+    }
+
+    private void setTitle(Intent intent, SettingInfo info) {
+        if (info != null) {
+            setTitle(info.title);
+        } else {
+            final int initialTitleResId = intent.getIntExtra(EXTRA_SHOW_FRAGMENT_TITLE_RESID, -1);
+            if (initialTitleResId > 0) {
+                setTitle(getResources().getString(initialTitleResId));
+            } else {
+                String title = intent.getStringExtra(EXTRA_SHOW_FRAGMENT);
+                setTitle(title != null ? title : getTitle());
+            }
+        }
     }
 }
