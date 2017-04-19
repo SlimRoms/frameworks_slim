17a133cc9a0a057a0ad31f7c546c8b1e831be9a5
Griffin Millender <griffinn.millender@gmail.com>
2017-04-18 22:21:16 -0500

    Settings Cleanup
    Change-Id: I9a83e344a28ebc74879b39843d57a541a1662a7c

--- packages/SlimSettings/src/com/slim/settings/SettingsActivity.java
+++ packages/SlimSettings/src/com/slim/settings/SettingsActivity.java
@@ -19,10 +19,13 @@ package com.slim.settings;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.preference.Preference;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.preference.PreferenceScreen;
+import android.text.TextUtils;
 import android.view.MenuItem;
 
 import com.slim.settings.Utils;
@@ -32,11 +35,24 @@ public class SettingsActivity extends SettingsDrawerActivity implements
         PreferenceFragment.OnPreferenceStartFragmentCallback,
         PreferenceFragment.OnPreferenceStartScreenCallback {
 
+    public static final String META_DATA_KEY_FRAGMENT_CLASS =
+            "com.slim.settings.FRAGMENT_CLASS";
+    public static final String META_DATA_KEY_PREFRERENCE_XML =
+            "com.slim.settings.PREFERENCE_XML";
+
+    private String mFragmentClass;
+    private String mPreferenceXML;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getMetaData();
+
         Fragment fragment = getFragment();
+        if (mFragmentClass != null) {
+            fragment = Fragment.instantiate(this, mFragmentClass);
+        }
         if (fragment != null) {
             FragmentTransaction transaction = getFragmentManager().beginTransaction();
             if (getFragmentBundle() != null) {
@@ -44,9 +60,19 @@ public class SettingsActivity extends SettingsDrawerActivity implements
             }
             transaction.replace(R.id.content_frame, fragment);
             transaction.commit();
-            showMenuIcon();
         }
         getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void getMetaData() {
+        try {
+            ActivityInfo ai = getPackageManager().getActivityInfo(getComponentName(),
+                    PackageManager.GET_META_DATA);
+            if (ai == null || ai.metaData == null) return;
+            mFragmentClass = ai.metaData.getString(META_DATA_KEY_FRAGMENT_CLASS);
+            mPreferenceXML = ai.metaData.getString(META_DATA_KEY_PREFRERENCE_XML);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
     }
 
     @Override
@@ -102,6 +128,11 @@ public class SettingsActivity extends SettingsDrawerActivity implements
     }
 
     public Bundle getFragmentBundle() {
+        if (!TextUtils.isEmpty(mPreferenceXML)) {
+            Bundle b = new Bundle();
+            b.putString("preference_xml", mPreferenceXML);
+            return b;
+        }
         return null;
     }
 }
