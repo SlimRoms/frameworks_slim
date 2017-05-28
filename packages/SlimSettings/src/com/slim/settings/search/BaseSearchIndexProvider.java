SlimRoms/frameworks_opt_slim

Add search to slimsettings
Change-Id: Ib16a7d5653e2b57fffcf916eca5f73e86418c954

packages/SlimSettings/src/com/slim/settings/search/BaseSearchIndexProvider.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slim.settings.search;
+
+import android.content.Context;
+
+import java.util.Set;
+
+/**
+ * Convenience class which can be used to return additional search metadata without
+ * having to implement all methods.
+ */
+public class BaseSearchIndexProvider implements Searchable.SearchIndexProvider {
+
+    @Override
+    public Set<String> getSearchKeywords(Context context) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getNonIndexableKeys(Context context) {
+        return null;
+    }
+}

SlimRoms/frameworks_slim

packages/SlimSettings/src/com/slim/settings/search/BaseSearchIndexProvider.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slim.settings.search;
+
+import android.content.Context;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Convenience class which can be used to return additional search metadata without
+ * having to implement all methods.
+ */
+public class BaseSearchIndexProvider implements Searchable.SearchIndexProvider {
+
+    @Override
+    public List<SearchIndexableRaw> getRawDataToIndex(Context context) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getNonIndexableKeys(Context context) {
+        return null;
+    }
+}