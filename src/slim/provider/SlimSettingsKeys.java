402f81415256fade13a68f0b8f6f445916ba49cf
Griffin Millender <griffinn.millender@gmail.com>
2017-11-11 11:14:56 -0600

    SlimSettings: Update SlimSettings for Oreo
    Change-Id: If29538e1cacd99fd29cde47b734c8fe1ce67f13b

--- src/slim/provider/SlimSettingsKeys.java
+++ src/slim/provider/SlimSettingsKeys.java
@@ -546,6 +546,27 @@ public final class SlimSettingsKeys {
          */
         public static final String PROXIMITY_ON_WAKE = "proximity_on_wake";
 
+       /**
+        * The keyboard brightness to be used while the screen is on.
+        * Valid value range is between 0 and {@link PowerManager#getMaximumKeyboardBrightness()}
+        * @hide
+        */
+        public static final String KEYBOARD_BRIGHTNESS = "keyboard_brightness";
+
+        /**
+        * The button brightness to be used while the screen is on or after a button press,
+        * depending on the value of {@link BUTTON_BACKLIGHT_TIMEOUT}.
+        * Valid value range is between 0 and {@link PowerManager#getMaximumButtonBrightness()}
+        * @hide
+        */
+        public static final String BUTTON_BRIGHTNESS = "button_brightness";
+
+        /**
+        * The time in ms to keep the button backlight on after pressing a button.
+        * A value of 0 will keep the buttons on for as long as the screen is on.
+        * @hide
+        */
+        public static final String BUTTON_BACKLIGHT_TIMEOUT = "button_backlight_timeout";
     }
 
     public interface Secure {
